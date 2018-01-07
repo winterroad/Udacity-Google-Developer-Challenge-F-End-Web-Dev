2019864539f36cb9dff97a026c5813445b686cf9
wintterroad <winterroaduk@gmail.com>
2018-01-07 23:52:14 +0200

    Added umbrella.js

---
+++ Javascript/umbrella.js
@@ -0,0 +1,28 @@
+/*
+ * Programming Quiz: Umbrella (7-1)
+ * Follow the example open() method and create the close() method.
+ */
+
+var umbrella = {
+    color: "pink",
+    isOpen: true,
+    open: function() {
+        if (umbrella.isOpen === true) {
+          return "The umbrella is already opened!";
+        } else{
+            umbrella.isOpen = true;
+            return "Julia opens the umbrella!";
+          }
+    },
+
+    close: function() {
+        if(umbrella.isOpen === false){
+            return "the Umbrella is already closed!";
+        }
+            else{
+                umbrella.isOpen = false;
+                return "Julia closes the umbrella!";
+
+            }
+    }
+};
